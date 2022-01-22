bc642c29488b15077c5313b304a1fd6f070467e7
Noah <41527027+ncwardell@users.noreply.github.com>
2022-01-22 13:33:27 -0500

    Update ProtocolDistributor.sol

--- ProtocolContracts/ProtocolDistributor.sol
+++ ProtocolContracts/ProtocolDistributor.sol
@@ -27,7 +27,7 @@ interface IProtocolERC20 {
     function burn(address account_, uint256 amount_) external returns(bool);
     function protocolToReserve(uint256 _amount) external view returns(uint);
     function reserveToProtocol(uint256 _amount) external view returns(uint);
-    function addToReservoir(uint _amount) external view returns(bool);
+    function addToReservoir(uint _amount) external returns(bool);
     function protocolReservoir() external view returns(uint);
     function owner() external view returns (address);
     function transferOwnership(address newOwner) external;
